6e8b5c6f1dce5f69e25bf83c64b84728c5edfbeb
Leif Ringstad <leifcr@gmail.com>
2025-02-04 23:19:39 +0100

    Add postgres builds

--- update.sh
+++ update.sh
@@ -4,12 +4,10 @@ cp -f Gemfile* ./dev/
 cp -f Gemfile* ./dev-mysql8/
 cp -f Gemfile* ./dev-no-entrypoint/
 cp -f Gemfile* ./mysql8-client/
-# cd dev
-# yarn upgrade
-# cp yarn.lock ../dev-no-entrypoint/
-# cp yarn.lock ../dev-mysql8/
-# yarn outdated
-# cd ..
+cd postgres
+bundle update
+cp -f Gemfile* ../dev-postgres/
+cd ..
 bundle outdated --strict
 
 # gc -m "Update gems"
