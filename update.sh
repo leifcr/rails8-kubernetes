d6eb6a9fe74c9de2667048588b4e18bc4e6919a8
Leif Ringstad <leifcr@gmail.com>
2026-01-20 20:50:42 +0100

    Upgrade packages

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-bundle update
+bundle update --all
 cp -f Gemfile* ./dev/
 cp -f Gemfile* ./dev-mysql8/
 cp -f Gemfile* ./dev-no-entrypoint/
 cp -f Gemfile* ./mysql8-client/
 cd postgres
-bundle update
+bundle update --all
 cp -f Gemfile* ../dev-postgres/
 cd ..
 bundle outdated --strict
